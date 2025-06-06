fc6834a8f96e306a71a8b871422f7f4bcdc4c8b8
Elizaveta Strelnikova <elizaveta.strelnikova357@gmail.com>
2025-06-06 13:43:57 +0300

    add migration

--- BookService/AppDbContext.cs
+++ BookService/AppDbContext.cs
@@ -1,6 +1,25 @@
-﻿namespace BookService
+﻿using BookService.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookService
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Конфигурация модели (опционально)
+            modelBuilder.Entity<Book>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Title).IsRequired();
+            });
+        }
     }
 }
--- BookService/Program.cs
+++ BookService/Program.cs
@@ -1,6 +1,14 @@
+﻿using BookService;
 using BookService.Services;
+using Microsoft.EntityFrameworkCore;
+using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("Postgres")));
+
+//builder.Services.AddSingleton<IConnectionMultiplexer>(ConnectionMultiplexer.Connect("Redis"));
 
 // Add services to the container.
 builder.Services.AddGrpc();
@@ -8,7 +16,7 @@ builder.Services.AddGrpc();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
+app.MapGrpcService<BooksService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
